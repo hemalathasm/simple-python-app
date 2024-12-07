90c26e44143faf5de51e9c3c7834491dacd876e6
hemalathasm <69075439+hemalathasm@users.noreply.github.com>
2024-12-07 18:05:55 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-echo
+docker pull hemalatha932/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 hemalatha932/simple-python-flask-app:latest
